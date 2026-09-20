65ba8f5a46d92e79bbba702586bad62e537c7d68
vijay sharma <vijaythaska1@gmail.com>
2026-09-21 00:21:32 +0530

    style: add space inside empty constructor of app controller

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -22,7 +22,7 @@ export class AppController {
   constructor(
     private readonly appService: AppService,
     private readonly configService?: ConfigService,
-  ) {}
+  ) { }
 
   @Get()
   getHello(): string {
